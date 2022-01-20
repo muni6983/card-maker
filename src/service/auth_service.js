ebf8d09300ed8adf3c2bc13ba9fd805a79016b93
gyeongmun <bada6983@gmail.com>
2022-01-20 22:47:34 +0900

    add login

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,11 +1,25 @@
 import firebase from "firebase/compat/app";
-// import { GoogleAuthProvider } from "firebase/auth";
-// import { GithubAuthProvider } from "firebase/auth";
+import firebaseApp from "./firebase";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
 
 class AuthService {
   login(providerName) {
-    const authProvider = new firebase.auth[`${providerName}AuthProvide`]();
-    return firebase.auth().signInWithPopup(authProvider);
+    let provider;
+    if (providerName === "Google") {
+      provider = new GoogleAuthProvider();
+    }
+    if (providerName === "Github") {
+      provider = new GithubAuthProvider();
+    }
+    const auth = getAuth();
+    return signInWithPopup(auth, provider);
+    // const authProvider = new firebase.auth[`${providerName}AuthProvide`]();
+    // return firebaseApp.auth().signInWithPopup(authProvider);
   }
 }
 
