1b59baeb0242ddf7e54f974190439e748b026653
gyeongmun <bada6983@gmail.com>
2022-01-22 00:58:56 +0900

    add button & modify cardEditForm

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import Button from "../button/button";
 import styles from "./card_edit_form.module.css";
-const CardEditForm = ({ card }) => <h1>{card.id}</h1>;
+const CardEditForm = ({ card }) => {
+  const { name, company, title, email, message, theme, fileURL } = card;
+  const onSubmit = () => {};
+  return (
+    <form>
+      <input type="text" name="name" value={name} />
+      <input type="text" name="company" value={company} />
+      <select name="theme" value={theme}>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="colorful">Colorful</option>
+      </select>
+      <input type="text" name="title" value={title} />
+      <input type="text" name="email" value={email} />
+      <textarea name="message" value={message}></textarea>
+      <Button name="Delete" onClick={onSubmit} />
+    </form>
+  );
+};
 
 export default CardEditForm;
